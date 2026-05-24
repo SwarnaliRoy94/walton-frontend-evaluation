f974c51d92ad33683f01bff41db8ada84b7cc4bf
Swarnali Roy <swarnali.roy093@gmail.com>
2026-05-24 21:55:35 +0600

    chore: configure Apollo client and ApolloWrapper with env setup #1

---
+++ src/lib/apolloWrapper.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { ApolloProvider } from "@apollo/client";
+import { apolloClient } from "./apollo";
+
+export default function ApolloWrapper({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ApolloProvider client={apolloClient}>
+      {children}
+    </ApolloProvider>
+  );
+}
