c9d809b341e5da4ba01b8f23e205431b09496455
Swarnali Roy <swarnali.roy093@gmail.com>
2026-05-24 22:00:55 +0600

    chore: add typed GraphQL queries for PLP and PDP closes #1

---
+++ src/graphql/queries.ts
@@ -0,0 +1,90 @@
+import { gql } from "@apollo/client";
+
+export const GET_PRODUCTS = gql`
+  query GetProducts($pagination: PaginationInput, $filter: ProductFilterInput) {
+    getProducts(pagination: $pagination, filter: $filter) {
+      message
+      statusCode
+      result {
+        count
+        products {
+          uid
+          enName
+          images {
+            url
+          }
+          variants {
+            mrpPrice
+            ebsItemCode
+            posItemCode
+            quantity
+            discount {
+              amount
+              value
+              type
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const GET_PRODUCT_DETAIL = gql`
+  query GetProductDetail($filter: ProductFilterInput) {
+    getProducts(filter: $filter, pagination: { skip: 0, limit: 1 }) {
+      message
+      statusCode
+      result {
+        products {
+          uid
+          enName
+          images {
+            url
+          }
+          productAttributes {
+            enLabel
+            values {
+              enName
+            }
+          }
+          detailedDescriptions {
+            enLabel
+            values {
+              enName
+            }
+          }
+          deliveries {
+            enLabel
+            values {
+              enName
+            }
+          }
+          serviceAndDeliveries {
+            enLabel
+            values {
+              enName
+            }
+          }
+          priceAndStocks {
+            enLabel
+            values {
+              enName
+            }
+          }
+          variants {
+            mrpPrice
+            ebsItemCode
+            posItemCode
+            quantity
+            discount {
+              amount
+              value
+              type
+            }
+          }
+        }
+      }
+    }
+  }
+`;
